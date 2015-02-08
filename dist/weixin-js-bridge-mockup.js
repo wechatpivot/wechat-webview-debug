dfe88562f3dfaa95a1eb0086699513ad15c2251f
Albert <shuaizhe.xu@nexttao.com>
2015-02-08 13:50:15 +0800

    每个 API 都是独立的，腾讯就没想过要提供格式统一的接口

--- dist/weixin-js-bridge-mockup.js
+++ dist/weixin-js-bridge-mockup.js
@@ -1,16 +1,25 @@
 var Bridge = {
   invoke: function (api_name, conf, callback) {
-    var MOCKUP_RESULT = window.WEIXIN_JS_BRIDGE_INVOKE || {};
-
-    callback({
+    var result = {
       err_code: 'WILL_DELETE',
       err_desc: 'WILL_DELETE',
       err_detail: 'WILL_DELETE',
 
       err_msg: 'ok',
+    };
 
-      resultStr: MOCKUP_RESULT[api_name] || '{}'
-    });
+    var MOCKUP_RESULT = window.WEIXIN_JS_BRIDGE_INVOKE || {};
+
+    if (api_name === 'geoLocation') {
+      var mock = MOCKUP_RESULT[api_name] || {};
+      for (var k in mock) {
+        result[k] = mock[k];
+      }
+    } else {
+      result.resultStr = MOCKUP_RESULT[api_name] || '{}';
+    }
+
+    callback(result);
   },
 
   on: function (api_name, callback) {
